8c8cb60403f831b45b0bc3f7102e51d6341bd921
sreeparvathyns <sreparvathynsn@gmail.com>
2025-07-17 07:53:27 +0530

    done

--- app.js
+++ app.js
@@ -1,43 +1,32 @@
-const express = require("express");
-const cors = require("cors");
+const mongoose=require("mongoose")
+const cors=require("cors")
+const express=require("express")
 
-const app = express();
+const app=express()
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
-app.post("/internalmark", (request, response) => {
-    const getName = request.body.name;
-    const getAdmissionNo = request.body.admno;
-    const getSubject = request.body.subject;
+app.post("/calculate",(request,response)=>{
+    const name=(request.body.name)
+    const admno=(request.body.admno)
+    const batch=(request.body.batch)
+    const present =parseInt(request.body.present)
+    const totalPresent=parseInt(request.body.totalp)
+    const exam1=parseInt(request.body.exam1)
+    const totalExam1=parseInt(request.body.totalExam1)
+    const exam2=parseInt(request.body.exam2)
+    const totalExam2=parseInt(request.body.totalExam2)
+    const assignment1=parseInt(request.body.assignment1)
+    const assignment2=parseInt(request.body.assignment2)
+    const attendance=(present/totalPresent)*8
+    const firstExam=(exam1/totalExam1)*10
+    const secondExam=(exam2/totalExam2)*10
+    const assignment=assignment1+assignment2
+    const total=attendance+firstExam+secondExam+assignment
 
-    const getPDay = parseInt(request.body.pdays);
-    const getTDay = parseInt(request.body.tdays);
-    const gDays = (getPDay / getTDay) * 8;
+    response.json({"internalMark":total,"attandance":attendance,"Exam":firstExam+secondExam,"assignment":assignment,"name":name,"admno":admno,"batch":batch})
+})
 
-    const getMark1 = parseInt(request.body.getMark1);
-    const getTmark1 = parseInt(request.body.getTmark);
-    const gMark1 = (getMark1 / getTmark1) * 10;
-
-    const getMark2 = parseInt(request.body.getMark2);
-    const getTMark2 = parseInt(request.body.getTMark2);
-    const gMark2 = (getMark2 / getTMark2) * 10;
-
-    const getAMark1 = parseInt(request.body.getAMark1);
-    const getAMark2 = parseInt(request.body.getAMark2);
-    const amark = getAMark1 + getAMark2
-
-    const total =  gMark1 + gMark2 + amark;
-
-    response.json({
-        name: getName,
-        admissionNo: getAdmissionNo,
-        subject: getSubject,
-        "Internal Mark": total
-    });
-});
-
-app.listen(4000, () => {
-    console.log("server is running on port 4000");
-});
+app.listen(4000,()=>{console.log("Server is running")})
